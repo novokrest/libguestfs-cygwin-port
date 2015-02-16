58847830be8e95dfd19ed0f6e9fe37e53905e39f
novokrest <novokrest013@gmail.com>
2015-02-16 15:32:38 +0300

    init commit

---
+++ libguestfs-1.28.5/v2v/input_libvirtxml.ml
@@ -0,0 +1,306 @@
+(* virt-v2v
+ * Copyright (C) 2009-2014 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Printf
+
+open Common_gettext.Gettext
+open Common_utils
+
+open Types
+open Utils
+
+type parsed_disk = {
+  p_source_disk : source_disk;
+  p_source : parsed_source;
+}
+and parsed_source =
+| P_source_dev of string
+| P_source_file of string
+| P_dont_rewrite
+
+let parse_libvirt_xml ~verbose xml =
+  if verbose then
+    printf "libvirt xml is:\n%s\n" xml;
+
+  let doc = Xml.parse_memory xml in
+  let xpathctx = Xml.xpath_new_context doc in
+
+  let xpath_to_string expr default =
+    let obj = Xml.xpath_eval_expression xpathctx expr in
+    if Xml.xpathobj_nr_nodes obj < 1 then default
+    else (
+      let node = Xml.xpathobj_node doc obj 0 in
+      Xml.node_as_string node
+    )
+  and xpath_to_int expr default =
+    let obj = Xml.xpath_eval_expression xpathctx expr in
+    if Xml.xpathobj_nr_nodes obj < 1 then default
+    else (
+      let node = Xml.xpathobj_node doc obj 0 in
+      let str = Xml.node_as_string node in
+      try int_of_string str
+      with Failure "int_of_string" ->
+        error (f_"expecting XML expression to return an integer (expression: %s)")
+          expr
+    )
+  in
+
+  let hypervisor = xpath_to_string "/domain/@type" "" in
+  let name = xpath_to_string "/domain/name/text()" "" in
+  let memory = xpath_to_int "/domain/memory/text()" (1024 * 1024) in
+  let memory = Int64.of_int memory *^ 1024L in
+  let vcpu = xpath_to_int "/domain/vcpu/text()" 1 in
+
+  if hypervisor = "" then
+    error (f_"in the libvirt XML metadata, <domain type='...'> is missing or empty");
+  let hypervisor = source_hypervisor_of_string hypervisor in
+
+  if name = "" then
+    error (f_"in the libvirt XML metadata, <name> is missing or empty");
+
+  let features =
+    let features = ref [] in
+    let obj = Xml.xpath_eval_expression xpathctx "/domain/features/*" in
+    let nr_nodes = Xml.xpathobj_nr_nodes obj in
+    for i = 0 to nr_nodes-1 do
+      let node = Xml.xpathobj_node doc obj i in
+      features := Xml.node_name node :: !features
+    done;
+    !features in
+
+  let display =
+    let obj = Xml.xpath_eval_expression xpathctx "/domain/devices/graphics" in
+    let nr_nodes = Xml.xpathobj_nr_nodes obj in
+    if nr_nodes < 1 then None
+    else (
+      (* Ignore everything except the first <graphics> device. *)
+      let node = Xml.xpathobj_node doc obj 0 in
+      Xml.xpathctx_set_current_context xpathctx node;
+      let keymap =
+        match xpath_to_string "@keymap" "" with "" -> None | k -> Some k in
+      let password =
+        match xpath_to_string "@passwd" "" with "" -> None | pw -> Some pw in
+      match xpath_to_string "@type" "" with
+      | "" -> None
+      | "vnc" ->
+        Some { s_display_type = `VNC;
+               s_keymap = keymap; s_password = password }
+      | "spice" ->
+        Some { s_display_type = `Spice;
+               s_keymap = keymap; s_password = password }
+      | "sdl"|"desktop" as t ->
+        warning (f_"virt-v2v does not support local displays, so <graphics type='%s'> in the input libvirt XML was ignored") t;
+        None
+      | t ->
+        warning (f_"display <graphics type='%s'> in the input libvirt XML was ignored") t;
+        None
+    ) in
+
+  (* Non-removable disk devices. *)
+  let disks =
+    let get_disks, add_disk =
+      let disks = ref [] and i = ref 0 in
+      let get_disks () = List.rev !disks in
+      let add_disk qemu_uri format controller p_source =
+        incr i;
+        disks :=
+          { p_source_disk = { s_disk_id = !i;
+                              s_qemu_uri = qemu_uri; s_format = format;
+                              s_controller = controller };
+            p_source = p_source } :: !disks
+      in
+      get_disks, add_disk
+    in
+    let obj =
+      Xml.xpath_eval_expression xpathctx
+        "/domain/devices/disk[@device='disk']" in
+    let nr_nodes = Xml.xpathobj_nr_nodes obj in
+    if nr_nodes < 1 then
+      error (f_"this guest has no non-removable disks");
+    for i = 0 to nr_nodes-1 do
+      let node = Xml.xpathobj_node doc obj i in
+      Xml.xpathctx_set_current_context xpathctx node;
+
+      let controller =
+        let target_bus = xpath_to_string "target/@bus" "" in
+        match target_bus with
+        | "" -> None
+        | "ide" -> Some `IDE
+        | "scsi" -> Some `SCSI
+        | "virtio" -> Some `Virtio_blk
+        | _ -> None in
+
+      let format =
+        match xpath_to_string "driver/@type" "" with
+        | "aio" -> Some "raw" (* Xen wierdness *)
+        | "" -> None
+        | format -> Some format in
+
+      (* The <disk type='...'> attribute may be 'block', 'file' or
+       * 'network'.  We ignore any other types.
+       *)
+      match xpath_to_string "@type" "" with
+      | "block" ->
+        let path = xpath_to_string "source/@dev" "" in
+        if path <> "" then
+          add_disk path format controller (P_source_dev path)
+      | "file" ->
+        let path = xpath_to_string "source/@file" "" in
+        if path <> "" then
+          add_disk path format controller (P_source_file path)
+      | "network" ->
+        (* We only handle <source protocol="nbd"> here, and that is
+         * intended only for virt-p2v.  Any other network disk is
+         * currently ignored.
+         *)
+        (match xpath_to_string "source/@protocol" "" with
+        | "nbd" ->
+          let host = xpath_to_string "source/host/@name" "" in
+          let port = xpath_to_int "source/host/@port" 0 in
+          if host <> "" && port > 0 then (
+            (* Generate a qemu nbd URL.
+             * XXX Quoting, although it's not needed for virt-p2v.
+             *)
+            let path = sprintf "nbd:%s:%d" host port in
+            add_disk path format controller P_dont_rewrite
+          )
+        | "" -> ()
+        | protocol ->
+          warning (f_"network <disk> with <source protocol='%s'> was ignored")
+            protocol
+        )
+      | disk_type ->
+        warning (f_"<disk type='%s'> was ignored") disk_type
+    done;
+    get_disks () in
+
+  (* Removable devices, CD-ROMs and floppy disks. *)
+  let removables =
+    let obj =
+      Xml.xpath_eval_expression xpathctx
+        "/domain/devices/disk[@device='cdrom' or @device='floppy']" in
+    let nr_nodes = Xml.xpathobj_nr_nodes obj in
+    let disks = ref [] in
+    for i = 0 to nr_nodes-1 do
+      let node = Xml.xpathobj_node doc obj i in
+      Xml.xpathctx_set_current_context xpathctx node;
+
+      let controller =
+        let target_bus = xpath_to_string "target/@bus" "" in
+        match target_bus with
+        | "" -> None
+        | "ide" -> Some `IDE
+        | "scsi" -> Some `SCSI
+        | "virtio" -> Some `Virtio_blk
+        | _ -> None in
+
+      let typ =
+        match xpath_to_string "@device" "" with
+        | "cdrom" -> `CDROM
+        | "floppy" -> `Floppy
+        | _ -> assert false (* libxml2 error? *) in
+
+      let disk =
+        { s_removable_type = typ; s_removable_controller = controller } in
+      disks := disk :: !disks
+    done;
+    List.rev !disks in
+
+  (* Network interfaces. *)
+  let nics =
+    let obj = Xml.xpath_eval_expression xpathctx "/domain/devices/interface" in
+    let nr_nodes = Xml.xpathobj_nr_nodes obj in
+    let nics = ref [] in
+    for i = 0 to nr_nodes-1 do
+      let node = Xml.xpathobj_node doc obj i in
+      Xml.xpathctx_set_current_context xpathctx node;
+
+      let mac = xpath_to_string "mac/@address" "" in
+      let mac =
+        match mac with
+        | ""
+        | "00:00:00:00:00:00" (* thanks, VMware *) -> None
+        | mac -> Some mac in
+
+      let vnet_type =
+        match xpath_to_string "@type" "" with
+        | "network" -> Some Network
+        | "bridge" -> Some Bridge
+        | _ -> None in
+      match vnet_type with
+      | None -> ()
+      | Some vnet_type ->
+        let vnet = xpath_to_string "source/@network | source/@bridge" "" in
+        if vnet <> "" then (
+          let nic = {
+            s_mac = mac;
+            s_vnet = vnet;
+            s_vnet_orig = vnet;
+            s_vnet_type = vnet_type
+          } in
+          nics := nic :: !nics
+        )
+    done;
+    List.rev !nics in
+
+  ({
+    s_hypervisor = hypervisor;
+    s_name = name; s_orig_name = name;
+    s_memory = memory;
+    s_vcpu = vcpu;
+    s_features = features;
+    s_display = display;
+    s_disks = [];
+    s_removables = removables;
+    s_nics = nics;
+   },
+   disks)
+
+class input_libvirtxml verbose file =
+object
+  inherit input verbose
+
+  method as_options = "-i libvirtxml " ^ file
+
+  method source () =
+    let xml = read_whole_file file in
+
+    let source, disks = parse_libvirt_xml ~verbose xml in
+
+    (* When reading libvirt XML from a file (-i libvirtxml) we allow
+     * paths to disk images in the libvirt XML to be relative (to the XML
+     * file).  Relative paths are in fact not permitted in real libvirt
+     * XML, but they are very useful when dealing with test images or
+     * when writing the XML by hand.
+     *)
+    let dir = Filename.dirname (absolute_path file) in
+    let disks = List.map (
+      function
+      | { p_source_disk = disk; p_source = P_dont_rewrite } -> disk
+      | { p_source_disk = disk; p_source = P_source_dev _ } -> disk
+      | { p_source_disk = disk; p_source = P_source_file path } ->
+        let path =
+          if not (Filename.is_relative path) then path else dir // path in
+        { disk with s_qemu_uri = path }
+    ) disks in
+
+    { source with s_disks = disks }
+end
+
+let input_libvirtxml = new input_libvirtxml
+let () = Modules_list.register_input_module "libvirtxml"
---
+++ libguestfs-1.28.5/v2v/output_libvirt.ml
@@ -0,0 +1,377 @@
+(* virt-v2v
+ * Copyright (C) 2009-2014 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Printf
+
+open Common_gettext.Gettext
+open Common_utils
+
+open Types
+open Utils
+open DOM
+
+module StringSet = Set.Make (String)
+
+let string_set_of_list =
+  List.fold_left (fun set x -> StringSet.add x set) StringSet.empty
+
+let target_features_of_capabilities_doc doc arch =
+  let xpathctx = Xml.xpath_new_context doc in
+  let expr =
+    (* NB: Pay attention to the square brackets.  This returns the
+     * <guest> nodes!
+     *)
+    sprintf "/capabilities/guest[arch[@name='%s']/domain/@type='kvm']" arch in
+  let obj = Xml.xpath_eval_expression xpathctx expr in
+
+  if Xml.xpathobj_nr_nodes obj < 1 then (
+    (* Old virt-v2v used to die here, but that seems unfair since the
+     * user has gone through conversion before we reach here.
+     *)
+    warning (f_"the target hypervisor does not support a %s KVM guest") arch;
+    []
+  ) else (
+    let node (* first matching <guest> *) = Xml.xpathobj_node doc obj 0 in
+    Xml.xpathctx_set_current_context xpathctx node;
+
+    (* Get guest/features/* nodes. *)
+    let obj = Xml.xpath_eval_expression xpathctx "features/*" in
+
+    let features = ref [] in
+    for i = 0 to Xml.xpathobj_nr_nodes obj - 1 do
+      let feature_node = Xml.xpathobj_node doc obj i in
+      let feature_name = Xml.node_name feature_node in
+      features := feature_name :: !features
+    done;
+    !features
+  )
+
+let append_child child = function
+  | PCData _ | Comment _  -> assert false
+  | Element e -> e.e_children <- e.e_children @ [child]
+
+let append_attr attr = function
+  | PCData _ | Comment _ -> assert false
+  | Element e -> e.e_attrs <- e.e_attrs @ [attr]
+
+let create_libvirt_xml ?pool source targets guestcaps target_features =
+  let memory_k = source.s_memory /^ 1024L in
+
+  (* We have the machine features of the guest when it was on the
+   * source hypervisor (source.s_features).  We have the acpi flag
+   * which tells us whether acpi is required by this guest
+   * (guestcaps.gcaps_acpi).  And we have the set of hypervisor
+   * features supported by the target (target_features).  Combine all
+   * this into a final list of features.
+   *)
+  let features = string_set_of_list source.s_features in
+  let target_features = string_set_of_list target_features in
+
+  (* If the guest supports ACPI, add it to the output XML.  Conversely
+   * if the guest does not support ACPI, then we must drop it.
+   * (RHBZ#1159258)
+   *)
+  let features =
+    if guestcaps.gcaps_acpi then
+      StringSet.add "acpi" features
+    else
+      StringSet.remove "acpi" features in
+
+  (* Make sure we don't add any features which are not supported by
+   * the target hypervisor.
+   *)
+  let features = StringSet.inter(*section*) features target_features in
+
+  (* But if the target supports apic or pae then we should add them
+   * anyway (old virt-v2v did this).
+   *)
+  let force_features = string_set_of_list ["apic"; "pae"] in
+  let force_features =
+    StringSet.inter(*section*) force_features target_features in
+  let features = StringSet.union features force_features in
+
+  let features = List.sort compare (StringSet.elements features) in
+
+  let disks =
+    let block_prefix =
+      match guestcaps.gcaps_block_bus with
+      | Virtio_blk -> "vd" | IDE -> "hd" in
+    let block_bus =
+      match guestcaps.gcaps_block_bus with
+      | Virtio_blk -> "virtio" | IDE -> "ide" in
+    mapi (
+      fun i t ->
+        e "disk" [
+          "type", if pool = None then "file" else "volume";
+          "device", "disk"
+        ] [
+          e "driver" [
+            "name", "qemu";
+            "type", t.target_format;
+            "cache", "none"
+          ] [];
+          (match pool with
+          | None ->
+            e "source" [
+              "file", absolute_path t.target_file;
+            ] []
+          | Some pool ->
+            e "source" [
+              "pool", pool;
+              "volume", Filename.basename t.target_file;
+            ] []
+          );
+          e "target" [
+            "dev", block_prefix ^ (drive_name i);
+            "bus", block_bus;
+          ] [];
+        ]
+    ) targets in
+
+  let removables =
+    (* CDs will be added as IDE devices if we're using virtio, else
+     * they will be added as the same as the disk bus.  The original
+     * s_removable_controller is ignored (same as old virt-v2v).
+     *)
+    let cdrom_bus, cdrom_block_prefix, cdrom_index =
+      match guestcaps.gcaps_block_bus with
+      | Virtio_blk | IDE -> "ide", "hd", ref 0
+      (* | bus -> bus, "sd", ref (List.length targets) *) in
+
+    (* Floppy disks always occupy their own virtual bus. *)
+    let fd_bus = "fdc" and fd_index = ref 0 in
+
+    List.map (
+      function
+      | { s_removable_type = `CDROM } ->
+        let i = !cdrom_index in
+        incr cdrom_index;
+        let name = cdrom_block_prefix ^ drive_name i in
+        e "disk" [ "device", "cdrom"; "type", "file" ] [
+          e "driver" [ "name", "qemu"; "type", "raw" ] [];
+          e "target" [ "dev", name; "bus", cdrom_bus ] []
+        ]
+
+      | { s_removable_type = `Floppy } ->
+        let i = !fd_index in
+        incr fd_index;
+        let name = "fd" ^ drive_name i in
+        e "disk" [ "device", "floppy"; "type", "file" ] [
+          e "driver" [ "name", "qemu"; "type", "raw" ] [];
+          e "target" [ "dev", name; "bus", fd_bus ] []
+        ]
+    ) source.s_removables in
+
+  let nics =
+    let net_model =
+      match guestcaps.gcaps_net_bus with
+      | Virtio_net -> "virtio" | E1000 -> "e1000" | RTL8139 -> "rtl8139" in
+    List.map (
+      fun { s_mac = mac; s_vnet_type = vnet_type;
+            s_vnet = vnet; s_vnet_orig = vnet_orig } ->
+        let vnet_type_str =
+          match vnet_type with
+          | Bridge -> "bridge" | Network -> "network" in
+
+        let nic =
+          let children = [
+            e "source" [ vnet_type_str, vnet ] [];
+            e "model" [ "type", net_model ] [];
+          ] in
+          let children =
+            if vnet_orig <> vnet then
+              Comment (sprintf "%s mapped from \"%s\" to \"%s\""
+                         vnet_type_str vnet_orig vnet) :: children
+            else
+              children in
+          e "interface" [ "type", vnet_type_str ] children in
+
+        (match mac with
+        | None -> ()
+        | Some mac ->
+          append_child (e "mac" [ "address", mac ] []) nic);
+
+        nic
+    ) source.s_nics in
+
+  (* Same as old virt-v2v, we always add a display here even if it was
+   * missing from the old metadata.
+   *)
+  let video, graphics =
+    let video, graphics =
+      match guestcaps.gcaps_video with
+      | QXL ->
+        e "video" [ "type", "qxl"; "ram", "65536" ] [],
+        e "graphics" [ "type", "vnc" ] []
+      | Cirrus ->
+        e "video" [ "type", "cirrus"; "vram", "9216" ] [],
+        e "graphics" [ "type", "spice" ] [] in
+
+    append_attr ("heads", "1") video;
+
+    append_attr ("autoport", "yes") graphics;
+    (match source.s_display with
+    | Some { s_keymap = Some km } -> append_attr ("keymap", km) graphics
+    | _ -> ());
+    (match source.s_display with
+    | Some { s_password = Some pw } -> append_attr ("passwd", pw) graphics
+    | _ -> ());
+
+    video, graphics in
+
+  let devices = disks @ removables @ nics @ [video] @ [graphics] @
+  (* Standard devices added to every guest. *) [
+    e "input" ["type", "tablet"; "bus", "usb"] [];
+    e "input" ["type", "mouse"; "bus", "ps2"] [];
+    e "console" ["type", "pty"] [];
+  ] in
+
+  let doc : doc =
+    doc "domain" [
+      "type", "kvm";                (* Always assume target is kvm? *)
+    ] [
+      e "name" [] [PCData source.s_name];
+      e "memory" ["unit", "KiB"] [PCData (Int64.to_string memory_k)];
+      e "currentMemory" ["unit", "KiB"] [PCData (Int64.to_string memory_k)];
+      e "vcpu" [] [PCData (string_of_int source.s_vcpu)];
+      e "os" [] [
+        e "type" ["arch", guestcaps.gcaps_arch] [PCData "hvm"];
+      ];
+      e "features" [] (List.map (fun s -> e s [] []) features);
+
+      e "on_poweroff" [] [PCData "destroy"];
+      e "on_reboot" [] [PCData "restart"];
+      e "on_crash" [] [PCData "restart"];
+
+      e "devices" [] devices;
+    ] (* /doc *) in
+
+  doc
+
+class output_libvirt verbose oc output_pool = object
+  inherit output verbose
+
+  val mutable capabilities_doc = None
+
+  method as_options =
+    match oc with
+    | None -> sprintf "-o libvirt -os %s" output_pool
+    | Some uri -> sprintf "-o libvirt -oc %s -os %s" uri output_pool
+
+  method prepare_targets source targets =
+    (* Get the capabilities from libvirt. *)
+    let cmd =
+      match oc with
+      | None -> "virsh capabilities"
+      | Some uri -> sprintf "virsh -c %s capabilities" (quote uri) in
+    if verbose then printf "%s\n%!" cmd;
+    let xml = external_command ~prog cmd in
+    let xml = String.concat "\n" xml in
+
+    if verbose then printf "libvirt capabilities XML:\n%s\n%!" xml;
+
+    (* This just checks that the capabilities XML is well-formed,
+     * early so that we catch parsing errors before conversion.
+     *)
+    let doc = Xml.parse_memory xml in
+
+    (* Stash the capabilities XML, since we cannot get the bits we
+     * need from it until we know the guest architecture, which happens
+     * after conversion.
+     *)
+    capabilities_doc <- Some doc;
+
+    (* Connect to output libvirt instance and check that the pool exists
+     * and dump out its XML.
+     *)
+    let xml = Domainxml.pool_dumpxml ?conn:oc output_pool in
+    let doc = Xml.parse_memory xml in
+    let xpathctx = Xml.xpath_new_context doc in
+
+    let xpath_to_string expr default =
+      let obj = Xml.xpath_eval_expression xpathctx expr in
+      if Xml.xpathobj_nr_nodes obj < 1 then default
+      else (
+        let node = Xml.xpathobj_node doc obj 0 in
+        Xml.node_as_string node
+      )
+    in
+
+    (* We can only output to a pool of type 'dir' (directory). *)
+    let pool_type = xpath_to_string "/pool/@type" "" in
+    if pool_type <> "dir" then
+      error (f_"-o libvirt: output pool '%s' is not a directory (type='dir').  See virt-v2v(1) section \"OUTPUT TO LIBVIRT\"") output_pool;
+    let target_path = xpath_to_string "/pool/target/path/text()" "" in
+    if target_path = "" || not (is_directory target_path) then
+      error (f_"-o libvirt: output pool '%s' has type='dir' but the /pool/target/path element either does not exist or is not a local directory.  See virt-v2v(1) section \"OUTPUT TO LIBVIRT\"") output_pool;
+
+    (* Set up the targets. *)
+    List.map (
+      fun t ->
+        let target_file =
+          target_path // source.s_name ^ "-" ^ t.target_overlay.ov_sd in
+        { t with target_file = target_file }
+    ) targets
+
+  method create_metadata source targets guestcaps _ =
+    (* We copied directly into the final pool directory.  However we
+     * have to tell libvirt.
+     *)
+    let cmd =
+      match oc with
+      | None -> sprintf "virsh pool-refresh %s" (quote output_pool)
+      | Some uri ->
+        sprintf "virsh -c %s pool-refresh %s"
+          (quote uri) (quote output_pool) in
+    if verbose then printf "%s\n%!" cmd;
+    if Sys.command cmd <> 0 then
+      warning (f_"could not refresh libvirt pool %s") output_pool;
+
+    (* Parse the capabilities XML in order to get the supported features. *)
+    let doc =
+      match capabilities_doc with
+      | None -> assert false
+      | Some doc -> doc in
+    let target_features =
+      target_features_of_capabilities_doc doc guestcaps.gcaps_arch in
+
+    (* Create the metadata. *)
+    let doc =
+      create_libvirt_xml ~pool:output_pool source targets
+        guestcaps target_features in
+
+    let tmpfile, chan = Filename.open_temp_file "v2vlibvirt" ".xml" in
+    DOM.doc_to_chan chan doc;
+    close_out chan;
+
+    (* Define the domain in libvirt. *)
+    let cmd =
+      match oc with
+      | None -> sprintf "virsh define %s" (quote tmpfile)
+      | Some uri ->
+        sprintf "virsh -c %s define %s" (quote uri) (quote tmpfile) in
+    if Sys.command cmd = 0 then (
+      try Unix.unlink tmpfile with _ -> ()
+    ) else (
+      warning (f_"could not define libvirt domain.  The libvirt XML is still available in '%s'.  Try running 'virsh define %s' yourself instead.")
+        tmpfile tmpfile
+    );
+end
+
+let output_libvirt = new output_libvirt
+let () = Modules_list.register_output_module "libvirt"
